d3d2a3c9b123361e9acf55b98ec5729d560ffe7e
Ezequiel-Benitez <ezequiel.benitez1993@gmail.com>
2024-05-01 18:19:06 -0300

    ej 3 comienzo

---
+++ TP4_GRUPO_5/TP4_GRUPO_5/ejercicio3.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TP4_GRUPO_5
+{
+    public partial class ejercicio3 : System.Web.UI.Page
+    {
+        private const string servidor = @"\SQLEXPRESS";
+        private const string urlBD = @"Data Source=" + servidor + ";Initial Catalog=Libreria;Integrated Security=True";
+        private string getTemas = "SELECT IdTema,Tema FROM Temas";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                /// Conexion a Base de Datos
+
+                SqlConnection connection = new SqlConnection(urlBD);
+                connection.Open();
+
+                /// Consulta Temas
+
+                SqlCommand sqlCommand = new SqlCommand(getTemas, connection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                /// Cargar el DropDownList
+
+                ddlTemas.DataSource = sqlDataReader;
+                ddlTemas.DataTextField = "Tema";
+                ddlTemas.DataValueField = "IdTema";
+                ddlTemas.DataBind();
+
+                connection.Close();
+            }
+        }
+        protected void ddlTemas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
